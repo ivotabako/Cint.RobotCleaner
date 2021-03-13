4b2b548145ec4dc5d719e73af3c7f095707958a4
ivotabako <ivotabakov1981@gmail.com>
2021-03-13 11:05:54 +0100

    3

--- Cint.RobotCleaner.Tests/RobotCleanerTests.cs
+++ Cint.RobotCleaner.Tests/RobotCleanerTests.cs
@@ -6,19 +6,40 @@ namespace Cint.RobotCleaner.Tests
 {
     public class RobotCleanerTests
     {
-        [Theory]
-        [InlineData(1, null, null, 0)]
-        public void CleanRoomTests(int numOfCommands, Tuple<int,int> initialPosition, List<Tuple<string, int>> commands, int expectedNumOfPlaces)
+        [Theory]     
+        //[MemberData(nameof(Data))]
+        [InlineData(0, null, null, 0)]
+        //[InlineData(0, new Tuple<int, int>(0, 0), null, 1)]
+        public void CleanRoomTests(int numOfCommands, Coordinate initialPosition, List<Tuple<string, int>> commands, int expectedNumOfPlaces)
         {
             var robot = new RobotCleaner();
             int numOfPlaces = robot.CleanRoom(numOfCommands, initialPosition, commands);
             Assert.Equal(expectedNumOfPlaces, numOfPlaces);
         }
+
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { 0, null, null, 0 },
+            new object[] { 0, new Tuple<int,int>(0,0), null, 1 },
+        };
     }
 
-    class RobotCleaner
+    [Serializable]
+    public class Coordinate
     {
-        internal int CleanRoom(int numOfCommands, Tuple<int, int> initialPosition, List<Tuple<string, int>> commands)
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int X { get; }
+        public int Y { get; }
+    }
+
+    public class RobotCleaner
+    {
+        internal int CleanRoom(int numOfCommands, Coordinate initialPosition, List<Tuple<string, int>> commands)
         {
             return 0;
         }
