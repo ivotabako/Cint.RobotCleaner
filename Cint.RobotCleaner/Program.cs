5466ac14d7e734398aa874ef5cf7710dc8e8fe79
ivotabako <ivotabakov1981@gmail.com>
2021-03-14 00:29:35 +0100

    20

--- Cint.RobotCleaner/Program.cs
+++ Cint.RobotCleaner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Cint.RobotCleaner
 {
@@ -6,7 +7,20 @@ namespace Cint.RobotCleaner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numOfCommands = Convert.ToInt32(Console.ReadLine());
+            string[] initialCoordinates = Console.ReadLine().Split(" ");
+            Coordinates initialPosition = new Coordinates(Convert.ToInt32(initialCoordinates[0]), Convert.ToInt32(initialCoordinates[1]));
+            List<Command> commands = new List<Command>();
+
+            for (int i = 0; i < numOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+                commands.Add(new Command((string)command[0], Convert.ToInt32(command[1])));
+            }
+
+            var robot = new RobotCleaner();
+            int numOfPlaces = robot.CleanRoom(numOfCommands, initialPosition, commands);
+            Console.WriteLine($"=> Cleaned: {numOfPlaces}");
         }
     }
 }
