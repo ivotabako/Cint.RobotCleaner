64954f1b021b306609fdb7845428f2a07a0c260c
ivotabako <ivotabakov1981@gmail.com>
2021-03-13 10:40:55 +0100

    2

--- Cint.RobotCleaner.Tests/RobotCleanerTests.cs
+++ Cint.RobotCleaner.Tests/RobotCleanerTests.cs
@@ -6,11 +6,21 @@ namespace Cint.RobotCleaner.Tests
 {
     public class RobotCleanerTests
     {
-        [Fact]
-        public void CleanRoomTests(int numOfCommands, Tuple<int,int> initialPosition, List<Tuple<string, int>> commands)
+        [Theory]
+        [InlineData(1, null, null, 0)]
+        public void CleanRoomTests(int numOfCommands, Tuple<int,int> initialPosition, List<Tuple<string, int>> commands, int expectedNumOfPlaces)
         {
             var robot = new RobotCleaner();
             int numOfPlaces = robot.CleanRoom(numOfCommands, initialPosition, commands);
+            Assert.Equal(expectedNumOfPlaces, numOfPlaces);
+        }
+    }
+
+    class RobotCleaner
+    {
+        internal int CleanRoom(int numOfCommands, Tuple<int, int> initialPosition, List<Tuple<string, int>> commands)
+        {
+            return 0;
         }
     }
 }
